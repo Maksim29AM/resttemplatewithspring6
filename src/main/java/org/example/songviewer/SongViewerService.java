76d0cf1c46bd82b08eaa7641ff2d935741ba0290
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-06-11 15:47:10 +0200

    list song fetchable

--- src/main/java/org/example/songviewer/SongViewerService.java
+++ src/main/java/org/example/songviewer/SongViewerService.java
@@ -2,28 +2,29 @@ package org.example.songviewer;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.example.itunes.proxy.ItunesResult;
-import org.example.itunes.service.ItunesService;
-import org.example.sampleshawnmendes.service.ShawnMendesService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SongViewerService {
 
-    private final ItunesService itunesService;
-    private final ShawnMendesService shawnMendesService;
+    private final List<SongFetchable> songFetchable;
 
-    SongViewerService(ItunesService itunesService, ShawnMendesService shawnMendesService) {
-        this.itunesService = itunesService;
-        this.shawnMendesService = shawnMendesService;
+    SongViewerService(List<SongFetchable> songFetchable) {
+        this.songFetchable = songFetchable;
     }
 
+//    private final SongFetchable songFetchable;
+//
+//    SongViewerService(SongFetchable songFetchable) {
+//        this.songFetchable = songFetchable;
+//    }
+
     public List<Song> viewAllSongs() {
-        List<Song> itunesResults = itunesService.fetchAllSongs();
-        List<Song> songs = shawnMendesService.fetchAllSongs();
         List<Song> songsToView = new ArrayList<>();
-        songsToView.addAll(itunesResults);
-        songsToView.addAll(songs);
+        songFetchable.forEach(
+                songService -> songsToView.addAll(songService.fetchAllSongs())
+        );
+//        songsToView.addAll(songFetchable.fetchAllSongs());
         return songsToView;
     }
 }
